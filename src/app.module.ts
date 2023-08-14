6d397460bbb66c52e37fde5ebbf8d69fc7b79655
Renata Alves <100436812+renataalvescun@users.noreply.github.com>
2023-08-14 12:59:08 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { StudentModule } from './student/student.module';
 import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
-  imports: [StudentModule, MongooseModule.forRoot( 'mongodb+srv://renata-alves:senha123@teacher-e.z32j3ri.mongodb.net/?retryWrites=true&w=majority' ), TeacherModule],
+  imports: [StudentModule, MongooseModule.forRoot( '' ), TeacherModule],
   controllers: [AppController],
   providers: [AppService],
 })
